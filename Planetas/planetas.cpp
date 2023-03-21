97094f4f7477c7aeab086b10394e77024208b33c
DavidMurilloCastellano <davidmuca02@gmail.com>
2023-03-21 10:18:51 +0100

    mod3

--- Planetas/planetas.cpp
+++ Planetas/planetas.cpp
@@ -16,30 +16,32 @@ using namespace std;
 //distancia media al Sol, velocidad orbital (ordenados según proximidad al Sol y expresados en unidades SI), en los
 //correspondientes reescalados según se indica en el guion. Solo se necesita emplear la primera vez que se ejecuta 
 //el programa. Los datos se han obtenido de: https://nssdc.gsfc.nasa.gov/planetary/factsheet/
-void reescala(void)
+//Argumentos: n, número de cuerpos que consideramos en nuestro sistema
+void reescala(int n)
 {
     ifstream fichIn;
     ofstream fichOut;
-    double datos[3][8], cte;
+    double datos[3][n], cte;
     int i,j;
 
     //Leemos los datos
     fichIn.open("condiciones_iniciales.txt");
     fichOut.open("datos.txt");
+    n--;
     while(!fichIn.eof())
     {
         if(!fichIn.is_open())
             cout << "Error al abrir el fichero";
         for (i=0; i<=2; i++)
         {
-            for(j=0; j<=7; j++)
+            for(j=0; j<=n; j++)
                 fichIn >> datos[i][j];
         }
     }
     
     //Aplicamos la transformación correspondiente a cada dato
    cte=sqrt(G*M/(c*c*c));
-   for(j=0;j<=7;j++)
+   for(j=0;j<=n;j++)
    {
         datos[0][j]=datos[0][j]/M;
         datos[1][j]=datos[1][j]/c;
@@ -50,7 +52,7 @@ void reescala(void)
     fichOut.precision(15); //Establecemos la precisión con la que se escriben los datos en el fichero
     for (i=0; i<=2; i++)
         {
-            for(j=0; j<=7; j++)
+            for(j=0; j<=n; j++)
                 fichOut << fixed << datos[i][j] << " ";
             fichOut << endl;
         }
@@ -65,15 +67,18 @@ void reescala(void)
 //están alineados en t=0 en el eje X. En "pos-vel_iniciales.txt" aparecen en la primera fila las coordenadas de
 //la posición inicial de cada planeta de la forma "x y" ordenados por proximidad al Sol. Análogo para la velocidad
 //en la segunda línea (todo en las unidades reescaladas)
-void PosVelInic(void)
+//Argumentos: n, número de cuerpos que consideramos en nuestro sistema
+void PosVelInic(int n)
 {
     ifstream fichIn;
     ofstream fichOut;
-    double datos[2][16];
-    int i,j;
+    double datos[2][2*n];
+    int i,j,m;
 
     //Inicializamos la matriz sabiendo que todos los planetas empiezan con y=0, v_x=0
-    for(j=0;j<=14;j=j+2)
+    m=2*n-1;
+    n--;
+    for(j=0;j<=m;j=j+2)
     {
         datos[0][j+1]=0.0;
         datos[1][j]=0.0;
@@ -86,19 +91,19 @@ void PosVelInic(void)
         if(!fichIn.is_open())
             cout << "Error al abrir el fichero";
 
-        for(j=0;j<=7;j++)
+        for(j=0;j<=n;j++)
         {
             fichIn >> datos[0][0]; //Descartamos las lecturas de las masas
         }
 
-        for(j=0;j<=14;j=j+2)
+        for(j=0;j<=m;j=j+2)
         {
             fichIn >> datos[0][j]; //Leemos las posiciones (eje X)
         }
 
-        for(j=1;j<=15;j=j+2)
+        for(j=1;j<=m;j=j+2)
         {
-            fichIn >> datos[1][j]; //Leemos las velocidades (sentido positivo del eje Y)
+            fichIn >> datos[1][j]; //Leemos las velocidades (sentido positivo del eje Y, giro antihorario)
         }
     }
 
@@ -106,7 +111,7 @@ void PosVelInic(void)
     fichOut.precision(15);
     for(i=0;i<=1;i++)
     {
-        for(j=0;j<=15;j++)
+        for(j=0;j<=m;j++)
             fichOut << fixed << datos[i][j] << " ";
         fichOut << endl;
     }
@@ -122,10 +127,10 @@ void PosVelInic(void)
 int main(void)
 {
     //Reescalamos los datos de las condiciones iniciales
-    //reescala();
+    //reescala(8);
 
     //Escribimos las posiciones y velocidades iniciales en el fichero correspondiente
-    //PosVelInic();
+    //PosVelInic(8);
 
 
     return 0;
