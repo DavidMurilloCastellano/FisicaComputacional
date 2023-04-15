148f5b72ce6e87cbb6ffc84edb1a2b790d2e581e
DavidMurilloCastellano <davidmuca02@gmail.com>
2023-04-15 08:54:29 +0200

    .

--- Ising/ising.cpp
+++ Ising/ising.cpp
@@ -6,24 +6,27 @@
 #include <cstdlib>
 using namespace std;
 
-#define rng true //Indica si la configuración inicial es aleatoria (true) u ordenada (false)
-#define N 100 //Número de nodos del sistema en cada eje
-#define pMC 100 //Número de pasos de Monte-Carlo que se dan
-#define T 1e-4 //Temperatura del sistema
+#define rng false //Indica si la configuración inicial es aleatoria (true) u ordenada (false)
+#define N 16 //Número de nodos del sistema en cada eje
+#define pMC 1e4 //Número de pasos de Monte-Carlo que se dan para calcular cada promedio de magnitudes
+#define T 1.5 //Temperatura del sistema
 
 //Cabecera con todas las funciones que hemos definido
 void conf_aleat(bool A[][N], int n);
 void conf_orden(bool A[][N],int n);
 //bool i2b(int x);
 int b2i(bool b);
-double energia(bool A[][N],int n, int m);
+int DEnergia(bool A[][N],int n, int m);
+int magn(bool A[][N]);
+int energia(bool A[][N]);
+int corr(bool A[][N], int i);
 
 int main (void)
 {
-    int i,j,k,n,m,M,L;
-    bool A[N][N];
-    double p;
-    ofstream fichOut;
+    int i1,i2,j,k,n,m,M,L,e;
+    bool A[N][N], x,y;
+    double p, mag, E, E2, cN;
+    ofstream fichODat, fichOMag;
     srand(time(NULL));
 
     //Elegimos una configuración inicial
@@ -33,31 +36,49 @@ int main (void)
         conf_orden(A,N);
     
     M=N*N; L=N-1;
-    fichOut.open("ising_data.dat");
-    for(i=0;i<pMC;i++)
+    mag=E=E2=0.0;
+    fichODat.open("ising_data.dat");
+    fichOMag.open("ising_magnitudes.txt");
+    for(i1=0;i1<100;i1++)
     {
-        //Volcamos los datos al fichero para representarlos luego
-        for(j=0;j<N;j++)
+        for(i2=0;i2<pMC;i2++)
         {
-            for(k=0;k<L;k++)
-                fichOut << b2i(A[j][k]) << ", ";
-            fichOut << b2i(A[j][L]) << endl;
-        }
-        fichOut << endl;
+            //Volcamos los datos al fichero para representarlos luego
+            for(j=0;j<N;j++)
+            {
+                for(k=0;k<L;k++)
+                    fichODat << b2i(A[j][k]) << ", ";
+                fichODat << b2i(A[j][L]) << endl;
+            }
+            fichODat << endl;
 
-        //Elegimos un nodo al azar y aplicamos el algoritmo
-        for(j=0; j<M; j++)
-        {
-            n=rand()%N; m=rand()%N;
-            p=exp(-energia(A,n,m)/T);
-            if(p>1 || rand()/RAND_MAX<p)
-                if(A[n][m])
-                    A[n][m]=false;
-                else
-                    A[n][m]=true;
+            //Elegimos un nodo al azar y aplicamos el algoritmo durante 1 pMC
+            for(j=0; j<M; j++)
+            {
+                n=rand()%N; m=rand()%N;
+                p=exp(((-1.0)*DEnergia(A,n,m))/T);
+                if(p>1 || rand()/RAND_MAX<p)
+                    if(A[n][m])
+                        A[n][m]=false;
+                    else
+                        A[n][m]=true;
+            }
+
+            //Calculamos las sumas que nos permitan hallar los promedios luego
+            mag=mag+magn(A);
+            e=energia(A); E=E+e; E2=E2+e*e;
         }
+
+        //Cálculo de los promedios de todas las magnitudes de interés
+        mag=mag/(M*pMC);
+        cN=(E2-(E*E)/pMC)/(4*M*T*pMC);
+        E=E/(4*N);
+                
+        //Pasar al fichero, ¿CÁLCULO VARIANZAS? ¿NECESITO cN?
+
     }
-    fichOut.close();
+    fichODat.close();
+    fichOMag.close();
 
     return 0;
 }
@@ -112,12 +133,12 @@ int b2i(bool b)
         return -1;
 }
 
-//Función energía: calcula la variación de energía correspondiente a la transición de un estado a otro
+//Función DEnergía: calcula la variación de energía correspondiente a la transición de un estado a otro
 //Argumentos:A[][], vector de espines; n,m, coordenadas del nodo seleccionado
 //Retorno: energía asociada a la transición
-double energia(bool A[][N],int n, int m)
+int DEnergia(bool A[][N],int n, int m)
 {
-    double e;
+    int e;
 
     if(n==N-1)
         e=b2i(A[0][m]);
@@ -142,4 +163,84 @@ double energia(bool A[][N],int n, int m)
     e=e*2*b2i(A[n][m]);
 
     return e;
+}
+
+//Función magn: devuelve la magnetización promedio (multiplicada por N^2 por optimización)
+//Argumentos: A[][], matriz con la orientación de los espines.
+//Retorno: valor de la magnetización
+int magn(bool A[][N])
+{
+    int i,j, sum;
+
+    sum=0;
+    for(i=0;i<N;i++)
+        for(j=0;j<N;j++)
+            sum=sum+b2i(A[i][j]);
+    
+    return sum;
+}
+
+//Función energia: devuelve el doble de la energía del sistema en un determinado instante
+//Argumentos: A[][], matriz con la orientación de los espines, 
+//Retorno: e, (doble) energía del sistema
+int energia(bool A[][N])
+{
+    int e,i,j;
+
+    e=b2i(A[0][0])*(b2i(A[0][1])+b2i(A[0,N-1])+b2i(A[1][0])+b2i(A[N-1][0])); //Energía nodo (0,0)
+
+    for(j=1;j<N-1;j++)
+        e=e+b2i(A[0][j])*(b2i(A[0][j+1])+b2i(A[0][j-1])+b2i(A[1][j])+b2i(A[N-1][j])); //Energía nodos (0,1),...,(0,N-2)
+    
+    e=e+b2i(A[0][N-1])*(b2i(A[0][0])+b2i(A[0][N-2])+b2i(A[1][N-1])+b2i(A[N-1][N-1])); //Energía nodo (0,N-1)
+
+    for(i=1;i<N-1;i++)
+    {
+        e=e+b2i(A[i][0])*(b2i(A[i][1])+b2i(A[i][N-1])+b2i(A[i+1][0])+b2i(A[i-1][0])); //Energía nodo (i,0)
+
+        for(j=1;j<N-1;j++)
+            e=e+b2i(A[i][j])*(b2i(A[i][j+1])+b2i(A[i][j-1])+b2i(A[i+1][j])+b2i(A[i-1][j])); //Energía nodos (i,1),...,(i,N-2)
+
+        e=e+b2i(A[i][N-1])*(b2i(A[i][0])+b2i(A[i][N-2])+b2i(A[i+1][N-1])+b2i(A[i-1][N-1])); //Energía nodo (i,N-1)
+    }
+
+    e=e+b2i(A[N-1][0])*(b2i(A[N-1][1])+b2i(A[N-1][N-1])+b2i(A[0][0])+b2i(A[N-2][0])); //Energía nodo (N-1,0)
+
+    for(j=1;j<N-1;j++)
+        e=e+b2i(A[N-1][j])*(b2i(A[N-1][j+1])+b2i(A[N-1][j-1])+b2i(A[0][j])+b2i(A[N-2][j])); //Energía nodos (N-1,1),...,(N-1,N-2)
+    
+    e=e+b2i(A[N-1][N-1])*(b2i(A[N-1][0])+b2i(A[N-1][N-2])+b2i(A[0][N-1])+b2i(A[N-2][N-1])); //Energía nodo (N-1,N-1)
+
+    return -e;
+}
+
+
+//Función corr: devuelve la correlación (multiplicada por N^2 por optimización)
+//Argumentos: A[][], matriz con la orientación de los espines; i, entrada de la función correlación
+//Retorno: valor de la correlación
+int corr(bool A[][N], int i)
+{
+    int sum, m,n,u;
+
+    sum=0; u=N-1;
+    if(0<=i && i<=u)
+    {
+        u=u-i;
+        for(m=0;m<N;m++)
+        {
+            for(n=0;n<=u;n++)
+                if(A[n][m]==A[n+i][m])
+                    sum=sum+1;
+                else
+                    sum=sum-1;
+
+            for(n=u+1;n<N;n++)
+                if(A[n][m]==A[n+i-N][m])
+                    sum=sum+1;
+                else
+                    sum=sum-1;
+        }
+    }
+    
+    return sum;
 }
