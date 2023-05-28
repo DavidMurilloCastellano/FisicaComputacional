bdce92c8c9b5764ed827de1bfc78e2ff28de6386
DavidMurilloCastellano <davidmuca02@gmail.com>
2023-05-28 11:26:02 +0200

    .

--- Termo-Ising/func-correlacion.cpp
+++ Termo-Ising/func-correlacion.cpp
@@ -8,8 +8,8 @@
 #include "gsl_rng.h" //Libreria para generación de números aleatorios
 using namespace std;
 
-#define N 32 //Número de nodos del sistema en cada eje
-#define P 16 //Número de puntos que se grafican
+#define N 128 //Número de nodos del sistema en cada eje
+#define P 32 //Número de puntos que se grafican
 #define pMC 1e4 //Número de pasos de Monte-Carlo que se dan para calcular cada promedio de magnitudes
 #define T1 1.5 //Extremo inferior del intervalo de temperaturas
 #define T2 3.5 //Extremo superior del intervalo de temperaturas
