4314cc4e344fd8615b2b86ad3bbb42a5cde20427
DavidMurilloCastellano <davidmuca02@gmail.com>
2023-06-12 08:15:30 +0200

    .

--- Termo-Ising/termo-ising.cpp
+++ Termo-Ising/termo-ising.cpp
@@ -8,12 +8,12 @@
 #include "gsl_rng.h" //Libreria para generación de números aleatorios
 using namespace std;
 
-#define N 16 //Número de nodos del sistema en cada eje
-#define ptos 100//Número de puntos a graficar
+#define N 128 //Número de nodos del sistema en cada eje
+#define D 100 //Cada cuantos puntos toma medidas para calcular los promedios correspondientes
 #define pMC 1e4 //Número de pasos de Monte-Carlo que se dan para calcular cada promedio de magnitudes
-#define T1 2.2 //Extremo inferior del intervalo de temperaturas
-#define T2 2.4 //Extremo superior del intervalo de temperaturas
-#define nT 3 //Número de valores de temperatura que se van a considerar en el intervalo [T1,T2]
+#define T1 1.5 //Extremo inferior del intervalo de temperaturas
+#define T2 3.5 //Extremo superior del intervalo de temperaturas
+#define nT 10 //Número de valores de temperatura que se van a considerar en el intervalo [T1,T2]
 
 //Cabecera con todas las funciones que hemos definido
 int b2i(bool b);
@@ -24,7 +24,7 @@ int corr(bool A[][N], int i);
 
 int main (void)
 {
-    double T, h, p, en, en2, sMag, sMag2, sE, sE2, sE4, mMag, mE, mE2, varMag, varE, varE2, aux;
+    double T, h, p, en, en2, sMag, sMag2, sE, sE2, sE4, mMag, mE, mE2, varMag, varE, varE2, aux, den;
     int i1,i2, M, L, n, m, j, k, mgn, e;
     bool A[N][N];
     ofstream fichO;
@@ -36,65 +36,66 @@ int main (void)
     gsl_rng_set(tau,semilla); //Inicializamos la semilla
     
     //Cálculo de constantes
-    M=N*N; L=N-1;
+    M=N*N; L=N-1; den=(pMC*1.0)/D;
     fichO.open("magnitudes-N="+to_string(N)+".txt");
-    
+
     //El algoritmo se ejecuta para cada una de las temperaturas consideradas
     h=(T2-T1)/(nT-1); 
     T=T1;
     for(k=0; k<nT; k++)
     {
-        
+        //Inicializamos las sumas
+        sMag=sMag2=sE=sE2=sE4=0.0;
+
         //Partimos de una configuración ordenada
         for(i1=0;i1<N;i1++)
             for(i2=0;i2<N;i2++)
                 A[i1][i2]=true;
-
+        
         //Calculamos el valor inicial de la energía del sistema
         en=energia(A);
-        fichO << T << endl;
-        for(i1=1;i1<=ptos;i1++) //Tendremos ptos puntos en las gráficas
-        {
-            //Inicializamos las sumas
-            sMag=sMag2=sE=sE2=sE4=0.0;
-            for(i2=0;i2<pMC;i2++)
-            {
-                //Elegimos un nodo al azar y aplicamos el algoritmo durante 1 pMC
-                for(j=0; j<M; j++)
-                {
-                    n=gsl_rng_uniform_int(tau,N); m=gsl_rng_uniform_int(tau,N);
-                    e=DEnergia(A,n,m);
-                    p=exp(-e/T);
-                    if(p>1 || gsl_rng_uniform(tau)<p)
-                    {
-                        en=en+e;
-                        if(A[n][m])
-                            A[n][m]=false;
-                        else
-                            A[n][m]=true;
-                    }
-                }
 
+        fichO << T ;
+        for(i2=1;i2<=pMC;i2++)
+        {
+            //Elegimos un nodo al azar y aplicamos el algoritmo durante 1 pMC
+            for(j=0; j<M; j++)
+            {
+                n=gsl_rng_uniform_int(tau,N); m=gsl_rng_uniform_int(tau,N);
+                e=DEnergia(A,n,m);
+                p=exp(-e/T);
+                if(p>1 || gsl_rng_uniform(tau)<p)
+                {
+                    en=en+e;
+                    if(A[n][m])
+                        A[n][m]=false;
+                    else
+                        A[n][m]=true;
+                }
+            }
+
+            if(i2%D==0)
+            {
                 //Calculamos las sumas para obtener los promedios posteriores
                 mgn=magn(A);
                 sMag=sMag+mgn; sMag2=sMag2+mgn*mgn;
                 en2=en*en;
-                sE=sE+en; sE2=sE2+en2; sE4=sE4+en2*en2;                
-            }
-            //Calculamos los promedios y las varianzas correspondientes
-            mMag=sMag/pMC; mE=sE/pMC; mE2=sE2/pMC; aux=mE*mE;
-            varMag=sMag2/pMC-mMag*mMag; varE=mE2-aux; varE2=sE4/pMC-mE2*mE2;
-             
-            //Volcamos al fichero los resultados obtenidos para representarlos luego
-            //Se ha aplicado un factor de cobertura correspondiente a un nivel de confianza del 95%
-            fichO << i1 << ", " << mMag/M << ", " << 1.96*sqrt(varMag/pMC)/M << ", "; //Cálculo de la magnetización promedio
-            fichO << mE/(2*N) << ", " << 1.96*sqrt(varE/pMC)/(2*N) << ", "; //Cálculo de la energía media
-            fichO << varE/(M*T) << ", " << 1.96*sqrt((varE2+4*aux*varE)/pMC)/(M*T) << endl;//Cálculo del calor específico   
-        }
-        fichO << endl;
+                sE=sE+en; sE2=sE2+en2; sE4=sE4+en2*en2;   
+            }             
 
-        T=T+h;
-       }
+        }
+        //Calculamos los promedios y las varianzas correspondientes
+        mMag=sMag/den; mE=sE/den; mE2=sE2/den; aux=mE*mE;
+        varMag=sMag2/den-mMag*mMag; varE=mE2-aux; varE2=sE4/den-mE2*mE2; 
+
+        //Volcamos al fichero los resultados obtenidos para representarlos luego
+        //Se ha aplicado un factor de cobertura correspondiente a un nivel de confianza del 95%
+        fichO << ", " << mMag/M << ", " << 1.96*sqrt(varMag/den)/M << ", "; //Cálculo de la magnetización promedio
+        fichO << mE/(2*N) << ", " << 1.96*sqrt(varE/den)/(2*N) << ", "; //Cálculo de la energía media
+        fichO << varE/(M*T) << ", " << 1.96*sqrt((varE2+4*aux*varE)/den)/(M*T) << endl;//Cálculo del calor específico 
+        
+        T=T+h; 
+    }
 
     fichO.close();
 
