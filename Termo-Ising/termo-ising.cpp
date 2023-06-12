a14309da953044b4f7f9fd02358ec0a988fcc3b2
DavidMurilloCastellano <davidmuca02@gmail.com>
2023-06-12 19:07:59 +0200

    .

--- Termo-Ising/termo-ising.cpp
+++ Termo-Ising/termo-ising.cpp
@@ -8,12 +8,12 @@
 #include "gsl_rng.h" //Libreria para generación de números aleatorios
 using namespace std;
 
-#define N 16 //Número de nodos del sistema en cada eje
+#define N 128 //Número de nodos del sistema en cada eje
 #define D 100 //Cada cuantos puntos toma medidas para calcular los promedios correspondientes
 #define pMC 1e6 //Número total de pasos de Monte-Carlo que se dan
 #define T1 1.5 //Extremo inferior del intervalo de temperaturas
 #define T2 3.5 //Extremo superior del intervalo de temperaturas
-#define nT 5 //Número de valores de temperatura que se van a considerar en el intervalo [T1,T2]
+#define nT 10 //Número de valores de temperatura que se van a considerar en el intervalo [T1,T2]
 
 //Cabecera con todas las funciones que hemos definido
 int b2i(bool b);
